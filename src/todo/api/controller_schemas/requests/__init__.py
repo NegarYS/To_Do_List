658ba5881110999ec1e39c01afe81e1ef5839a77
Negar Yeganeh <negaryeganeh2004@gmail.com>
2025-12-02 14:09:43 -0800

    chore(api): add __init__ exports for request schemas

---
+++ src/todo/api/controller_schemas/requests/__init__.py
@@ -0,0 +1,25 @@
+"""Export all request schemas."""
+
+from .project_requests import (
+    ProjectCreateRequest,
+    ProjectUpdateRequest
+)
+
+from .task_requests import (
+    TaskCreateRequest,
+    TaskUpdateRequest,
+    TaskStatusUpdateRequest,
+    TaskStatus
+)
+
+__all__ = [
+    # Project requests
+    "ProjectCreateRequest",
+    "ProjectUpdateRequest",
+
+    # Task requests
+    "TaskCreateRequest",
+    "TaskUpdateRequest",
+    "TaskStatusUpdateRequest",
+    "TaskStatus",
+]
